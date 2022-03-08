f1b606dd50487777a0af3653d0415cd42846f2c1
Obyvante <sorepeace@gmail.com>
2022-03-08 19:03:07 +0300

    - Added interface priority feature. - Fixed screen touch issue for interfaces.

--- src/shared/Library/Services/InterfaceService.lua
+++ src/shared/Library/Services/InterfaceService.lua
@@ -7,7 +7,11 @@ local Interface = require(game:GetService("ReplicatedStorage"):WaitForChild("Lib
 -- VARIABLES
 local _addons = {}
 local _interfaces = {}
-
+-- STATICS
+class.VIEWPORTS = {
+    PC = Vector2.new(3840, 2160),
+    MOBILE = Vector2.new(1366, 1024)
+}
 
 -- Gets addon by its name.
 -- @param _name Addon name.
@@ -69,14 +73,14 @@ end
 -- @param _id Interface id.
 -- @param _viewport Interface viewport. (BASED ON)
 -- @return Created interface.
-function class.createScreen(_id : string, _viewport : Vector2)
+function class.createScreen(_id : string, _viewport : Vector2, _proirty : number)
     -- Object nil check.
     assert(_id ~= nil, "Interface id cannot be null")
     assert(_viewport ~= nil, "Interface(" .. _id .. ") viewport cannot be null")
     assert(_interfaces[_id] == nil, "Interface(" .. _id .. ") is already exist")
 
     -- Creates an interface.
-    local interface = Interface.createScreen(_id, _viewport)
+    local interface = Interface.createScreen(_id, _viewport, _proirty)
     -- Adds created interface to the list.
     _interfaces[_id] = interface
 
@@ -110,6 +114,20 @@ function class.delete(_id : string)
 
     _interfaces[_id]:destroy()
     _interfaces[_id] = nil
+end
+
+
+----------
+-- UTILS
+----------
+
+-- Checks if it is clicked or not.
+-- @param _type Enum input type.
+-- @param _state Input state
+-- @return If it is clicked or not.
+function class.isClicked(_type : Enum, _state : Enum)
+    if _state ~= Enum.UserInputState.End then return false end
+    return _type == Enum.UserInputType.MouseButton1 or _type == Enum.UserInputType.Touch
 end
 
 
